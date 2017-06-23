2973a712fdeaa46af88eb8b5bd569fc13614081b
cvik1 <c@vik.org>
2017-06-23 11:05:37 -0400

    comments

--- roomMapping.ino
+++ roomMapping.ino
@@ -1,11 +1,14 @@
 /*
- * 
+ * The following code naviagates a robot along the right hand wall of a room
+ * using ultrasonic sensors, storing room data to a microSD card.
+ * This project is run on an Arduino Uno with a Adafruit motor shield, a
+ * microSD reader shield, two DC motos, and two ultrasonic sensors.
  */
 
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h> //include motor shield library
-#include "utility/Adafruit_MS_PWMServoDriver.h" 
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <EPROM.h> //include library to write to Arduino "hard drive"
 
 // initialize motor shield and motor objects
@@ -21,13 +24,13 @@ const int trig2 = 6;
 const int echo2 = 7;
 
 void setup() {
-  AFMS.begin(); // start motor shield 
+  AFMS.begin(); // start motor shield
   // define input/output pins for untrasonic sensors
   pinMode(trig1, OUTPUT);
   pinMode(echo1, INPUT_PULLUP);
   pinMode(trig2, OUTPUT);
   pinMode(echo2, INPUT_PULLUP);
-  
+
   myMotor1->setSpeed(100);
   myMotor2->setSpeed(100);
 
@@ -36,7 +39,7 @@ void setup() {
 void loop() {
   long duration1, duration2, front, right; // ultrasonic sensor varaibles
 
-  // send trigger signals 
+  // send trigger signals
   digitalWrite(trig1, LOW); // start low to clean noise
   delayMicroseconds(2);
   digitalWrite(trig1, HIGH); // high pulse
@@ -56,39 +59,39 @@ void loop() {
   // divide by 29 for speed of sounce 29cm/µs
 
   duration2 = pulseIn(echo2, HIGH); // read in off echo pin
-  right = (duration2/2) / 29; // distance to right wall 
+  right = (duration2/2) / 29; // distance to right wall
 
-  // if right wall is within range, and no object is close enough 
+  // if right wall is within range, and no object is close enough
   // in front, continue
   if (front > 10 && right < 10) {
     delay(40);
     myMotor1->run(FORWARD);
-    myMotor2->run(FORWARD); 
+    myMotor2->run(FORWARD);
     // TODO add movement to mapping data
-    
+
   }
 
-  // if right wall is too far, turn right and continue forward 
+  // if right wall is too far, turn right and continue forward
   else if (front > 10 && right > 10) {
-    myMotor1->run(FORWARD); // run left forward and right backward 
+    myMotor1->run(FORWARD); // run left forward and right backward
     myMotor2->run(BACKWARD); // to turn to the right
 
     myMotor1->run(FORWARD); // continue forward
-    myMotor2->run(BACKWARD); 
+    myMotor2->run(BACKWARD);
 
     // TODO add movement to mapping data
   }
-  
-  // if right wall is close and front is close, we have a corner so 
+
+  // if right wall is close and front is close, we have a corner so
   // turn left and continue forward
   else if (front < 10 && right < 10) {
-    myMotor1->run(BACKWARD); // run left back and right forward 
+    myMotor1->run(BACKWARD); // run left back and right forward
     myMotor2->run(FORWARD); // to turn to the left
 
     myMotor1->run(FORWARD); // continue forward
-    myMotor2->run(FORWARD); 
+    myMotor2->run(FORWARD);
 
     // TODO add movement to mapping data
   }
-  
+
 }
